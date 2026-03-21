b32e9f60f8d9e69ca6dd39218ac24476bc7c2946
pzimondi <pzimondi@byupathway.edu>
2026-03-21 09:05:10 +0200

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -101,12 +101,12 @@ export async function updateInvoice(
   redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string): Promise<void> {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
   } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+    console.error('Database Error:', error);
   }
 }
 
