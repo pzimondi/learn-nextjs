5b9b7b4868aa17936cd8753f6cad34d01f1e07ed
pzimondi <pzimondi@byupathway.edu>
2026-03-21 08:16:29 +0200

    login

--- auth.config.ts
+++ auth.config.ts
@@ -8,16 +8,14 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        return false;
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
-
       return true;
     },
   },
-  providers: [], // Add providers in auth.ts
+  providers: [],
 } satisfies NextAuthConfig;
--- proxy.ts
+++ proxy.ts
@@ -4,6 +4,5 @@ import { authConfig } from './auth.config';
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  // This matcher protects all /dashboard routes and sub-routes
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
